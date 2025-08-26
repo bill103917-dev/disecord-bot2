b7db799330090db19442eaac1c823dfccf00f8c1
bill103917-dev <bill103917@gmail.com>
2025-08-26 19:17:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,27 +2,13 @@ import os
 import discord
 from discord.ext import commands
 
-# 設定 Intents
 intents = discord.Intents.default()
-intents.message_content = True  # 讓 Bot 可以讀訊息內容
-
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# 上線事件
 @bot.event
 async def on_ready():
     print(f'Bot is ready! Logged in as {bot.user}')
 
-# 測試訊息
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content == "!ping":
-        await message.channel.send("Pong!")
-
-# 確認 Token 是否有載入
 TOKEN = os.getenv("DISCORD_TOKEN")
-print("Token loaded:", TOKEN is not None)
-
-bot.run(DISCORD_TOKEN)
+bot.run(TOKEN)
