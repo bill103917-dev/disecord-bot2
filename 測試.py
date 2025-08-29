e0f632a0cdacfd27d23d79d787ca4bbdfcb16e76
bill103917-dev <bill103917@gmail.com>
2025-08-29 22:32:29 +0800

    Update 測試.py

--- 測試.py
+++ 測試.py
@@ -1,17 +1,1 @@
-import discord
-from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True  # 重要: 讓機器人能讀取訊息內容
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user}')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-bot.run("你的TOKEN")
